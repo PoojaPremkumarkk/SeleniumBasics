a34df6b261321e830636f9630ba26cb0cab3ebf8
pooja <pooja@LAPTOP-FD43FT9U>
2024-07-30 22:09:53 +0530

    full

--- Seleniumbasics/src/test/java/org/TESTNGCommands/Commands.java
+++ Seleniumbasics/src/test/java/org/TESTNGCommands/Commands.java
@@ -5,42 +5,72 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class Commands extends Base {
 	@Test
 	public void verifyHomePageTitle()
 	{
-		driver.get("https://demowebshop.tricentis.com/");
-		String title=driver.getTitle();
-		System.out.println("Title" +title);
+		driver.get("https://demowebshop.tricentis.com");
+		String actualtitle=driver.getTitle();
+		//System.out.println("Title" +actualtitle);
+		String Exceptedtitle="demo Web Shop";
+		Assert.assertEquals(actualtitle, Exceptedtitle, "Title mismatch");
 
 		
 		
 }
 	@Test
-	public void demowebshop()
+	
+		public void demowebshop() {
+		    
+		    // Login email
+		    
+		    
+		    driver.navigate().to("https://demowebshop.tricentis.com/login");
+			
+		    WebElement loginemail=driver.findElement(By.id("Email"));
+		    loginemail.sendKeys("pooja@gmail.com");
+		    WebElement loginpassword=driver.findElement(By.id("Password"));
+		    loginpassword.sendKeys("pooja123");
+		    WebElement login=driver.findElement(By.className("login-button"));
+		    login.click();
+		    
+		    
+
+		    String expectedEmail = "pooja@gmail.com";
+		    WebElement emailid=driver.findElement(By.xpath("//a[text()='pooja@gmail.com']"));
+		    String actualEmail=emailid.getText();
+
+		    // Assertion
+		    Assert.assertEquals(actualEmail, expectedEmail, "Email id mismatch");
+		}
+	@Test
+	
+	public void swagLabs()
 	{
-		
+		driver.get("https://www.saucedemo.com/");
+		WebElement username=driver.findElement(By.xpath("//input[@id='user-name']"));
+		username.sendKeys("standard_user");
+		WebElement pass=driver.findElement(By.xpath("//input[@id='password']"));
+		pass.sendKeys("secret_sauce");
+		WebElement login=driver.findElement(By.xpath("//input[@id='login-button']"));
+		login.click();
+		String exceptedapplogo="Swag Labs";
+	WebElement actualapplogo=driver.findElement(By.xpath("//div[text()='Swag Labs']"));
+	String actualr=actualapplogo.getText();
+	Assert.assertEquals(actualr, exceptedapplogo, "This is not correct app logo");
+	}
 	
-	
-	WebElement register=driver.findElement(By.className("ico-register"));
-	register.click();
-	//register details
-	WebElement firstname=driver.findElement(By.id("FirstName"));
-	firstname.sendKeys("pooja");
-	WebElement lastname=driver.findElement(By.id("LastName"));
-	lastname.sendKeys("premkumar");
-	WebElement email=driver.findElement(By.id("Email"));
-	email.sendKeys("pooja@gmail.com");
-	//password
-	WebElement password=driver.findElement(By.id("Password"));
-	password.sendKeys("pooja123");
-	WebElement confirmpassword=driver.findElement(By.id("ConfirmPassword"));
-	confirmpassword.sendKeys("pooja123");
-	WebElement submitregister=driver.findElement(By.id("register-button"));
-	submitregister.click();
-	
-	
-	
-}}
+@Test
+public void verifyDemoworkshop()
+{
+	driver.get("https://demowebshop.tricentis.com/register");
+	WebElement check1=driver.findElement(By.xpath("//input[@id='gender-male']"));
+	boolean isCheckboxSelected=check1.isSelected();
+	//Assert.assertFalse(isCheckboxSelected, "check1 is selected");
+	Assert.assertTrue(isCheckboxSelected, "check1 is not selected");
+}
+}
+
